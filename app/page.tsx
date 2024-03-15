24be97194e5eaed5bc18399e245f736dacf5ec52
geekgirlmm <40037425+geekgirlmm@users.noreply.github.com>
2024-03-15 15:31:16 -0400

    Why yes, i did mean className

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
 
       
-          <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               5 Favorite Support tasks
             </h5>
-            <p class="font-normal text-gray-700 dark:text-gray-400">
+            <p className="font-normal text-gray-700 dark:text-gray-400">
               Manage a support team
               Run ticket review sessions to make sure tone is consistent
               Dig through logs to troubleshoot a customer&apos;'s broken project
@@ -19,11 +19,11 @@ export default function Home() {
             </p>
           </a>
 
-          <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               5 Least Favorite Support tasks
             </h5>
-            <p class="font-normal text-gray-700 dark:text-gray-400">
+            <p className="font-normal text-gray-700 dark:text-gray-400">
               Help resolve billing issues for customers
               Respond to queries on Twitter, Reddit, Hacker News and other 3rd party sites
               Create video tutorials to help teach users a specific feature or use case
